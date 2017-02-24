cf0f032e4c7d7c67f38e1624cd8698dbd677a0e8
Tomas Tomecek <ttomecek@redhat.com>
2017-02-24 18:37:48 +0100

    start structuring program
    Signed-off-by: Tomas Tomecek <ttomecek@redhat.com>

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,69 @@
 extern crate git2;
 
-use git2::Repository;
-use git2::BranchType;
+use git2::{Repository,Branch,BranchType,Oid,Reference};
 
 
-// fn get_current_branch_name(repo: Repository) -> String {
-//     let head = match repo.head() {
-//         Ok(head) => head,
-//         Err(e) => panic!("failed to get head: {}", e)
-//     };
-//     return head.name().unwrap().to_string();
-// }
+struct Program {
+    repo: Repository,
+}
 
+fn get_branch_remote(reference: Reference) -> Oid {
+    let b = Branch::wrap(reference);
+    let upstream = match b.upstream() {
+        Ok(u) => u,
+        Err(e) => panic!("Failed to get upstream: {}.", e)
+    };
+    match upstream.get().target() {
+        Some(o) => o,
+        None => panic!("Failed to get oid of branch.")
+    }
+}
+
+impl Program {
+    fn get_head(&self) -> Reference {
+        match self.repo.head() {
+            Ok(head) => head,
+            Err(e) => panic!("Failed to get head: {}.", e)
+        }
+    }
+
+    fn get_current_branch_oid(&self) -> Oid {
+        let head = self.get_head();
+        match head.target() {
+            Some(v) => v,
+            None => panic!("Failed to get reference to head."),
+        }
+    }
+
+    fn get_current_branch_name(&self) -> String {
+        match self.get_head().name() {
+            Some(v) => v.to_string(),
+            None => panic!("Failed to get name of head."),
+        }
+    }
+
+    // fn get_current_branch_remote(&self) -> String {
+    //     let oid = self.get_current_branch_oid();
+    //     let b = Branch::wrap(oid);
+    //     let upstream = b.upstream().ok().unwrap();
+    //     String::from(upstream.name().ok().unwrap().unwrap())
+    // }
+
+    fn get_current_branch_remote_oid(&self) -> Oid {
+        get_branch_remote(self.get_head())
+    }
+
+    fn get_current_branch_ahead_behind(&self) -> (usize, usize)  {
+        let res = self.repo.graph_ahead_behind(
+            self.get_current_branch_oid(),
+            self.get_current_branch_remote_oid(),
+        );
+        match res {
+            Ok(r) => r,
+            Err(e) => panic!("failed to compute ahead/behind: {}", e)
+        }
+    }
+}
 
 
 fn main() {
@@ -19,16 +71,14 @@ fn main() {
         Ok(repo) => repo,
         Err(e) => panic!("failed to open: {}", e),
     };
+    let program = Program{ repo: repo };
+    println!("{}", program.get_current_branch_name());
+    let (ahead, behind) = program.get_current_branch_ahead_behind();
+    println!("A: {}, B: {}", ahead, behind);
     // let current_branch_name = get_current_branch_name(repo);
     // println!("{:?}", current_branch_name);
-    let head = match repo.head() {
-        Ok(head) => head,
-        Err(e) => panic!("failed to get head: {}", e)
-    };
-    let oid = head.target().unwrap();
-    let us = match repo.find_branch("origin/master", BranchType::Remote) {
-        Ok(us) => us,
-        Err(e) => panic!("failed to find origin/master: {}", e)
-    };
-    repo.graph_ahead_behind(oid, us.get().target().unwrap());
+    // let us = match repo.find_branch("origin/master", BranchType::Remote) {
+    //     Ok(us) => us,
+    //     Err(e) => panic!("failed to find origin/master: {}", e)
+    // };
 }
