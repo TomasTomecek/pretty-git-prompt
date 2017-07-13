f24415280592c58c656ba452d45357883ea5ed0f
Tomas Tomecek <ttomecek@redhat.com>
2017-07-13 13:04:35 +0200

    ci: travis user has different id than container
    Signed-off-by: Tomas Tomecek <ttomecek@redhat.com>

--- hack/ci.sh
+++ hack/ci.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # Travis CI test runner
 
-
+sudo chmod -R 0777 .
+sudo chown -R 1000:1000 .
 if [ "${TRAVIS_EVENT_TYPE}" = "cron" -a "${TARGET}" = "x86_64-unknown-linux-gnu" ] ; then
   make nightly-environment && make test
 else
